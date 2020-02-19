82331e01f11f7f58b41fc693bc31c5911cd65ed4
vincent-castanie <vincgonzo@hotmail.fr>
2020-02-19 15:17:28 +0100

    test update on main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main()
     ChaineDeCharacter ch;
     ChaineDeCharacter ch0("test");
     ChaineDeCharacter ch2("Hello");
+    ChaineDeCharacter chInt("12");
 
 
     if(ch0 != ch2)
@@ -28,7 +29,29 @@ int main()
 
     cout << ch2 << endl;
     ch2.ajout_fin('!');
-    cout << ch2 << endl;
+    cout << ch2 << endl << endl;
 
+    cout << "==========================" << endl;
+    cout << "======= Test running =====" << endl;
+    cout << "==========================" << endl;
+
+    if(ch0 >= ch2)
+        cout << "ch0 is superior or equal to ch2" << endl;
+    if(ch2 >= ch0)
+        cout << "ch2 is superior or equal to ch0" << endl;
+    if(ch0 <= ch2)
+        cout << "ch0 is inferior or equal to ch2" << endl;
+    if(ch2 <= ch0)
+        cout << "ch2 is inferior or equal to ch0" << endl;
+    if(ch0 > ch2)
+        cout << "ch0 is superior to ch2" << endl;
+    if(ch2 > ch0)
+        cout << "ch2 is superior to ch0" << endl;
+    if(ch0 < ch2)
+        cout << "ch0 is inferior to ch2" << endl;
+    if(ch2 < ch0)
+        cout << "ch2 is inferior to ch0" << endl;
+
+    cout << chInt + 1 << endl;
     return 0;
 }
