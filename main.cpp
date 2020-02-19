d5138ad3dfb458ac0ae884b6b3a1e9ad855bac75
vincent-castanie <vincgonzo@hotmail.fr>
2020-02-19 13:18:09 +0100

    test main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,19 @@ int main()
     cout << "Hello world!" << endl;
 
     ChaineDeCharacter ch;
+    ChaineDeCharacter ch0("test");
     ChaineDeCharacter ch2("Hello");
+
+
+    if(ch0 != ch2)
+        cout << "pas bien !!";
 
     cout << ch.longueur() << endl;
     ch = ch2;
+    if(ch == ch2)
+        cout << "egaux";
 
-    cout << ch << endl;
-    cout << ch2 << endl;
+    ch.mettreMaj();
 
     cout << "==========================" << endl;
     ch2 += ch;
