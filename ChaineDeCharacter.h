8dc2e4520fd51313c32d118e9066ab50998a7ecf
vincent-castanie <vincgonzo@hotmail.fr>
2020-03-11 14:23:30 +0100

    rewrite operator with friend .h

--- ChaineDeCharacter.h
+++ ChaineDeCharacter.h
@@ -9,6 +9,7 @@ class ChaineDeCharacter
 {
     int m_long;
     char *m_car;
+        int compare(ChaineDeCharacter const& ch) const;
 
     public:
         ChaineDeCharacter();
@@ -18,21 +19,21 @@ class ChaineDeCharacter
         unsigned int longueur() const;
         void ajout_fin(char c);
         void mettreMaj();
-        int compare(ChaineDeCharacter const& ch) const;
-        bool memeLongueur(ChaineDeCharacter const& ch) const;
 
         ChaineDeCharacter& operator+=(ChaineDeCharacter const& ChaineDeCharacterToCopy);
         ChaineDeCharacter& operator=(ChaineDeCharacter const& ChaineDeCharacterToCopy);
+        char operator[](unsigned int const c) const;
         operator int() const;
 
         friend std::ostream& operator<<(std::ostream &flux, ChaineDeCharacter const& a);
+
+        friend bool operator==(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
+        friend bool operator!=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
+        friend bool operator>=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
+        friend bool operator<=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
+        friend bool operator>(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
+        friend bool operator<(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
 };
 
-bool operator==(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
-bool operator!=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
-bool operator>=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
-bool operator<=(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
-bool operator>(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
-bool operator<(ChaineDeCharacter const& a, ChaineDeCharacter const& b);
 
 #endif // CHAINEDECHARACTER_H_INCLUDED
